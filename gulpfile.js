4728f0c67478f7b6f8b58c10087bbe518861f466
Chris Gmyr <cmgmyr@gmail.com>
2015-05-21 14:23:06 -0400

    add prismjs files, refs #4

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,22 @@ var elixir = require('laravel-elixir');
 
 var paths = {
     'bootstrap': 'vendor/bower_components/bootstrap/',
-    'fontawesome': 'vendor/bower_components/fontawesome/',
-    'jquery': 'vendor/bower_components/jquery/dist/'
+    'jquery': 'vendor/bower_components/jquery/dist/',
+    'fontawesome': 'vendor/bower_components/fontawesome/'
 };
 
 elixir(function(mix) {
     mix.less('app.less')
 
     .scripts([
-        paths.jquery + "jquery.js",
-        paths.bootstrap + "dist/js/bootstrap.js",
-        "resources/assets/js/app.js"
-    ], 'public/js/app.js', "./")
+        paths.jquery + 'jquery.js',
+        paths.bootstrap + 'dist/js/bootstrap.js',
+        'resources/assets/js/prism.js',
+        'resources/assets/js/app.js'
+    ], 'public/js/app.js', './')
 
     .copy(paths.bootstrap + 'fonts/**', 'public/fonts')
     .copy(paths.fontawesome + 'fonts/**', 'public/fonts')
 
-    .version(["css/app.css", "js/app.js"]);
+    .version(['css/app.css', 'js/app.js']);
 });
