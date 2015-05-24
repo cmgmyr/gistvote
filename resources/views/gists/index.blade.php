d807ab6fb2d1e906d1efad9b3f590cf0e86f9bac
Chris Gmyr <cmgmyr@gmail.com>
2015-05-24 11:31:28 -0400

    updated views with new gist object info, refs #5

--- resources/views/gists/index.blade.php
+++ resources/views/gists/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <h1>Your Gists <a href="{{ route('gists.refresh') }}" class="refresh" title="Refresh Gists"><i class="fa fa-refresh"></i></a></h1>
 
-    @if(count($gists))
+    @if($gists->count() > 0)
         <ul id="gist_list" class="list-group">
-        @foreach($gists as $gist)
-            @include('gists/partials/list-gist', $gist)
+        @foreach($gists->all() as $gist)
+            @include('gists/partials/list-gist', ['gist' => $gist])
         @endforeach
         </ul>
     @endif
