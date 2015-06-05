6ffd9b1119a63a201306d5662e4d2f377ff2c202
Chris Gmyr <cmgmyr@gmail.com>
2015-06-05 16:07:06 -0400

    add basic vote parsing, refs #11

--- app/Gists/GistComment.php
+++ app/Gists/GistComment.php
@@ -30,6 +30,11 @@ class GistComment
      */
     public $updated_at;
 
+    /**
+     * @var null|string
+     */
+    private $vote = null;
+
     public function __construct($id, array $user, $body, $created_at, $updated_at)
     {
         $this->id = $id;
@@ -37,6 +42,8 @@ class GistComment
         $this->body = $body;
         $this->created_at = Carbon::parse($created_at);
         $this->updated_at = Carbon::parse($updated_at);
+
+        $this->parseForVotes();
     }
 
     /**
@@ -77,5 +84,21 @@ class GistComment
     public function renderHtml()
     {
         return Parser::transform($this->body);
+    }
+
+    /**
+     * This will update the comment vote if there is a -1 or +1 found.
+     * We'll take the positive vote in case there is an off chance
+     * where both are found
+     */
+    private function parseForVotes()
+    {
+        if (str_contains($this->body, '-1')) {
+            $this->vote = 'n';
+        }
+
+        if (str_contains($this->body, '+1')) {
+            $this->vote = 'y';
+        }
     }
 }
