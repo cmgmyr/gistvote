587e2c34836a052f6a053ffd322fb7a6600ec296
Chris Gmyr <cmgmyr@gmail.com>
2015-07-19 14:23:11 -0400

    added listener for new event

---
+++ app/Listeners/AddPoweredByComment.php
@@ -0,0 +1,28 @@
+<?php namespace Gistvote\Listeners;
+
+use Gistvote\Events\GistWasActivated;
+use Gistvote\Services\GitHub;
+
+class AddPoweredByComment
+{
+
+    /**
+     * Handle the event.
+     *
+     * @param GistWasActivated $event
+     */
+    public function handle(GistWasActivated $event)
+    {
+        $gist = $event->gist;
+        $gitHub = new GitHub($gist->user);
+
+        if (!$gist->has_powered_by) {
+            $gistUrl = route('gists.show', ['username' => $gist->user->username(), 'id' => $gist->id]);
+            $comment = 'This gist has vote tracking powered by [Gist.vote](' . $gistUrl . ')';
+
+            $gitHub->gistComment($gist->id, $comment);
+            $gist->has_powered_by = true;
+            $gist->save();
+        }
+    }
+}
