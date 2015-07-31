00d728e2526c673345a8248b040774bddabd306a
Chris Gmyr <cmgmyr@gmail.com>
2015-07-31 07:44:42 -0400

    split up eloquent and GH tests

--- tests/GistTest.php
+++ tests/GistTest.php
@@ -27,13 +27,17 @@ class GistTest extends TestCase
     }
 
     /** @test */
-    public function it_should_return_first_file()
+    public function it_should_return_first_file_eloquent()
     {
         $gistFromEloquent = Gist::fromEloquent($this->buildEloquentGist());
 
         $this->assertInstanceOf(GistFile::class, $gistFromEloquent->firstFile());
+    }
 
-        list($eloquentGist, $githubGist) = $this->buildGithubGist(['id' => 2, 'email' => 'chris@gist.vote'], ['user_id' => 2]);
+    /** @test */
+    public function it_should_return_first_file_gh()
+    {
+        list($eloquentGist, $githubGist) = $this->buildGithubGist(['id' => 2, 'email' => 'chris@gist.vote'], ['id' => '5300bf315d8f29864d9b2', 'user_id' => 2]);
         $gistFromGithub = Gist::fromGitHub($eloquentGist, $githubGist);
 
         $this->assertInstanceOf(GistFile::class, $gistFromGithub->firstFile());
