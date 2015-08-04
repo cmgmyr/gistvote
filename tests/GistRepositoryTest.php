73333d65359f23da058614c760d4f135f5725f16
Chris Gmyr <cmgmyr@gmail.com>
2015-08-04 16:59:34 -0400

    added test for creating a new eloquent gist

--- tests/GistRepositoryTest.php
+++ tests/GistRepositoryTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class GistRepositoryTest extends TestCase
 {
-    use DatabaseMigrations;
+    use DatabaseMigrations, GistFixtureHelpers;
 
     /** @test */
     public function it_should_return_all_gists_from_a_user()
@@ -22,5 +22,18 @@ class GistRepositoryTest extends TestCase
 
         $this->assertCount(2, $gists);
         $this->assertInstanceOf(Gistvote\Gists\Gist::class, $gists->first());
+    }
+
+    /** @test */
+    public function it_should_create_a_new_eloquent_gist_from_api()
+    {
+        $user = factory(User::class)->create();
+
+        $githubGist = $this->loadFixture('5300bf315d8f29864d9b.json');
+
+        $repo = new \Gistvote\Gists\GistRepository();
+        $gist = $repo->findByIdOrCreate($githubGist, $user->id);
+
+        $this->assertInstanceOf(Gistvote\Gists\Gist::class, $gist);
     }
 }
