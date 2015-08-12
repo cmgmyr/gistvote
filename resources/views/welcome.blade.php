3bbcd8d6cdc19e08b2725bb427b4bfa7d97a1a9f
Chris Gmyr <cmgmyr@gmail.com>
2015-08-11 20:25:23 -0400

    add new, basic landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to GistVote!</h1>
-    <p>Your gists turned into a voting platform. (coming soon)</p>
+    <div class="jumbotron">
+        <h1>GistVote</h1>
+        <p class="lead">Ever have a great idea you wanted to share with others? GistVote makes it simple for you to share and tally your voter's responses. For free!</p>
+        <p><a class="btn btn-lg btn-success" href="{{ route('login') }}" role="button"><i class="fa fa-github"></i> Login with GitHub</a></p>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-4">
+            <h2>Easy to Use</h2>
+            <p>You already have a GitHub account, why not make your gists more powerful?</p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Get Your Idea Across</h2>
+            <p>Share and enable voting for any of your current gists. We'll tally up the totals for you so you'll know where your idea stands.</p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Free</h2>
+            <p>Yes, the best things in life are actually free!</p>
+        </div>
+    </div>
 @stop
