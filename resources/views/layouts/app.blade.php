62bab115117d98d4e7f2dd5bb14657940aab8fe3
Chris Gmyr <cmgmyr@gmail.com>
2015-08-22 16:34:43 -0400

    add meta section to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GistVote - Turn Your Gists Into a Voting Platform</title>
+    @yield('meta')
 
     <link rel="stylesheet" href="{{ elixir("css/app.css") }}">
 
