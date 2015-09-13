17b674a77fcfaa9f1df56d00b095d13eb9e272a3
Chris Gmyr <cmgmyr@gmail.com>
2015-09-12 20:21:47 -0400

    update js with voting options text handling, closes #23

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,4 +12,21 @@ $(function () {
             console.log('oops, something went wrong!');
         });
     });
+
+    $('#vote-buttons .btn').click(function() {
+        var vote = $(this).data('vote');
+        var comment = $('#comment-box');
+        var commentText = comment.val();
+
+        // search/replace all voting options
+        commentText = commentText.replace(':+1:', '');
+        commentText = commentText.replace('+1', '');
+        commentText = commentText.replace(':-1:', '');
+        commentText = commentText.replace('-1', '');
+
+        // append new voting option
+        comment.val(':' + vote + '1: ' + commentText.trim()).focus();
+
+        return false;
+    });
 });
